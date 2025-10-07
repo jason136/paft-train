64f400c26cb2603e6cc09f41c5213f00330e9e1f
jason136 <jason@together.ai>
2025-10-06 22:09:07 -0700

    mujoco xml

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
+"""
+Get started with your own first training loop
+=============================================
+
+**Author**: `Vincent Moens <https://github.com/vmoens>`_
+
+.. _gs_first_training:
+
+.. note:: To run this tutorial in a notebook, add an installation cell
+  at the beginning containing:
+
+    .. code-block::
+
+        !pip install tensordict
+        !pip install torchrl
+
+"""
+
+#################################
+# Time to wrap up everything we've learned so far in this Getting Started
+# series!
+#
+# In this tutorial, we will be writing the most basic training loop there is
+# using only components we have presented in the previous lessons.
+#
+# We'll be using DQN with a CartPole environment as a prototypical example.
+#
+# We will be voluntarily keeping the verbosity to its minimum, only linking
+# each section to the related tutorial.
+#
+# Building the environment
+# ------------------------
+#
+# We'll be using a gym environment with a :class:`~torchrl.envs.transforms.StepCounter`
+# transform. If you need a refresher, check our these features are presented in
+# :ref:`the environment tutorial <gs_env_ted>`.
+#
+
 import torch
 
 torch.manual_seed(0)
@@ -6,10 +44,22 @@ import time
 
 from torchrl.envs import GymEnv, StepCounter, TransformedEnv
 
-env = TransformedEnv(GymEnv("CartPole-v1"), StepCounter())
+env = TransformedEnv(GymEnv("CartPole-v1"), StepCounter(max_steps=1000))
 env.set_seed(0)
 
 from tensordict.nn import TensorDictModule as Mod, TensorDictSequential as Seq
+
+#################################
+# Designing a policy
+# ------------------
+#
+# The next step is to build our policy.
+# We'll be making a regular, deterministic
+# version of the actor to be used within the
+# :ref:`loss module <gs_optim>` and during
+# :ref:`evaluation <gs_logging>`.
+# Next, we will augment it with an exploration module
+# for :ref:`inference <gs_storage>`.
 
 from torchrl.modules import EGreedyModule, MLP, QValueModule
 
@@ -21,6 +71,15 @@ exploration_module = EGreedyModule(
 )
 policy_explore = Seq(policy, exploration_module)
 
+
+#################################
+# Data Collector and replay buffer
+# --------------------------------
+#
+# Here comes the data part: we need a
+# :ref:`data collector <gs_storage_collector>` to easily get batches of data
+# and a :ref:`replay buffer <gs_storage_rb>` to store that data for training.
+#
 
 from torchrl.collectors import SyncDataCollector
 from torchrl.data import LazyTensorStorage, ReplayBuffer
@@ -35,10 +94,16 @@ collector = SyncDataCollector(
     total_frames=-1,
     init_random_frames=init_rand_steps,
 )
-rb = ReplayBuffer(storage=LazyTensorStorage(100_000))
+rb = ReplayBuffer(storage=LazyTensorStorage(100_000_000))
 
 from torch.optim import Adam
 
+#################################
+# Loss module and optimizer
+# -------------------------
+#
+# We build our loss as indicated in the :ref:`dedicated tutorial <gs_optim>`, with
+# its optimizer and target parameter updater:
 
 from torchrl.objectives import DQNLoss, SoftUpdate
 
@@ -46,7 +111,12 @@ loss = DQNLoss(value_network=policy, action_space=env.action_spec, delay_value=T
 optim = Adam(loss.parameters(), lr=0.02)
 updater = SoftUpdate(loss, eps=0.99)
 
-
+#################################
+# Logger
+# ------
+#
+# We'll be using a CSV logger to log our results, and save rendered videos.
+#
 
 from torchrl._utils import logger as torchrl_logger
 from torchrl.record import CSVLogger, VideoRecorder
@@ -55,10 +125,22 @@ path = "./training_loop"
 logger = CSVLogger(exp_name="dqn", log_dir=path, video_format="mp4")
 video_recorder = VideoRecorder(logger, tag="video")
 record_env = TransformedEnv(
-    GymEnv("CartPole-v1", from_pixels=True, pixels_only=False), video_recorder
+    TransformedEnv(
+        GymEnv("CartPole-v1", from_pixels=True, pixels_only=False),
+        StepCounter(max_steps=1000),
+    ),
+    video_recorder,
 )
 
-
+#################################
+# Training loop
+# -------------
+#
+# Instead of fixing a specific number of iterations to run, we will keep on
+# training the network until it reaches a certain performance (arbitrarily
+# defined as 200 steps in the environment -- with CartPole, success is defined
+# as having longer trajectories).
+#
 
 total_count = 0
 total_episodes = 0
@@ -84,7 +166,7 @@ for i, data in enumerate(collector):
                 torchrl_logger.info(f"Max num steps: {max_length}, rb length {len(rb)}")
             total_count += data.numel()
             total_episodes += data["next", "done"].sum()
-    if max_length >= 1000:
+    if max_length > 1000:
         break
 
 t1 = time.time()
@@ -93,7 +175,23 @@ torchrl_logger.info(
     f"solved after {total_count} steps, {total_episodes} episodes and in {t1-t0}s."
 )
 
-
+#################################
+# Rendering
+# ---------
+#
+# Finally, we run the environment for as many steps as we can and save the
+# video locally (notice that we are not exploring).
 
 record_env.rollout(max_steps=1000, policy=policy)
 video_recorder.dump()
+
+#################################
+#
+# This is what your rendered CartPole video will look like after a full
+# training loop:
+#
+# .. figure:: /_static/img/cartpole.gif
+#
+# This concludes our series of "Getting started with TorchRL" tutorials!
+# Feel free to share feedback about it on GitHub.
+#
